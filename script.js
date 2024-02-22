2e3599fd84aa3556e6024fe120490bb460e97ebe
bhupendradhami <dhamibhupendra3@gmail.com>
2024-02-22 16:57:32 +0530

    1.0.0

---
+++ script.js
@@ -0,0 +1,14 @@
+// fetch('https://icanhazdadjoke.com/slack')
+//     .then(data => data.json())
+//     .then(jokeData => {
+//         const txt = jokeData.attachments[0].text;
+//         const display = document.getElementById('joke');
+//         display.innerHTML = txt;
+//     });
+
+
+// fetch("https://dog.ceo/api/breeds/image/random")
+//     .then(response => response.json())
+//     .then(data => {
+//         document.body.style.backgroundImage = `url('${data.message}')`;
+//     });
