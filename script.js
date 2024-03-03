a0fdb362259a60513f2c5c827ae2b52bd9932a4e
Bhupender Singh <dhamibhupendra3@gmail.com>
2024-03-03 10:08:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,6 @@ fetch("https://dog.ceo/api/breeds/image/random")
     .then(response => response.json())
     .then(data => {
         document.body.style.backgroundImage = `url('${data.message}')`;
+        docuement.body.style.backgroundSize ='cover';
+        docuement.body.style.backgroundPosition ='center center';
     });
